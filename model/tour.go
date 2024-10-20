cf6b6516206938ca77f8d04452d857c0bcd4f1a0
leedrum <levantungat@gmail.com>
2024-10-20 16:15:35 +0700

    CRUD tours

---
+++ model/tour.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Tour struct {
+	gorm.Model
+	ID          int    `gorm:"primaryKey" json:"id"`
+	Name        string `json:"name" form:"name"`
+	Description string `json:"description" form:"description"`
+	Status      int    `json:"status" form:"status"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+const (
+	TourStatusInActive = 0
+	TourStatusActive   = 1
+)
+
+func (t *Tour) GetStatusStr() string {
+	switch t.Status {
+	case TourStatusInActive:
+		return "InActive"
+	case TourStatusActive:
+		return "Active"
+	default:
+		return "Unknown"
+	}
+}
