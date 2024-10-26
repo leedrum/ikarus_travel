92ee766156845252aa67b5af0043b12533026270
leedrum <levantungat@gmail.com>
2024-10-27 02:31:07 +0700

    protect cookies

--- internal/session.go
+++ internal/session.go
@@ -1,10 +1,23 @@
 package internal
 
 import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 )
 
 func (server *Server) InitSessionStore() {
 	store := cookie.NewStore([]byte("secret"))
+	secureSession := false
+	if server.Config.Environment == "production" {
+		secureSession = true
+	}
+	store.Options(sessions.Options{
+		HttpOnly: true,
+		Secure:   secureSession, // Set to true if your website is served over HTTPS
+		SameSite: http.SameSiteLaxMode,
+	})
+
 	server.SessionStore = &store
 }
